a88932520e04baad2230abab5ac604616ceb3904
Timo Puschkasch <timo@puschkasch.com>
2017-01-04 23:49:22 +0100

    Identified unsupported python versions

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=requirements,
     classifiers=[
         'Framework :: Flask',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: BSD License'
     ]
